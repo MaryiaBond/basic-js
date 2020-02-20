70c110672d5a6294157f0b866d20e328768773c9
MaryiaBond <dairymolko@gamil.com>
2020-02-20 12:54:51 +0300

    feat: done carbon-dating task & count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,11 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let x
+  if ((typeof sampleActivity) === "string")
+    x = Math.ceil(-(HALF_LIFE_PERIOD/0.693) * Math.log(parseFloat(sampleActivity)/MODERN_ACTIVITY))
+  return (x > 0 && x < 60000) ? x : false
+  // -(T1/2/0.693) * ln(A/Ao)
+  // -(5730/0.693) * Math.log(sampleActivity/15)
 };
