5bd59a81d2995be3a42504f717bc1bb065f04b3e
MaryiaBond <dairymolko@gamil.com>
2020-02-21 17:57:57 +0300

    feat: start recursive-depth task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,38 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, {...props}) {
+    let separator = '+'
+    let additionSeparator = '|'
+    let repeatTimes = 1
+    let additionRepeatTimes = 1
+    if (props.separator) {
+        separator = props.separator
+    }
+    if (props.additionSeparator) {
+        additionSeparator = props.additionSeparator
+    }
+    if (props.repeatTimes) {
+        repeatTimes = props.repeatTimes
+    }
+    if (props.additionRepeatTimes) {
+        additionRepeatTimes = props.additionRepeatTimes
+    }
+
+    let result = ''
+
+        for (let i = 0; i < repeatTimes; i++) {
+            result += str
+
+            if(props.addition !== undefined) {
+                result += props.addition
+                for (let i = 0; i < additionRepeatTimes - 1; i++) {
+                    result += additionSeparator + props.addition
+                }
+            }
+
+
+            if (i < props.repeatTimes - 1)
+                result += separator
+        }
+
+    return result
 };
   
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,33 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor() {
+        this.deep = 1;
+        this.currentDeep = 0;
     }
-};
+    deepOfElement(arr) {
+        this.currentDeep++
+        let arrFlat = arr.flat(arr)
+        console.log(arrFlat)
+        let arrFilter = arr.filter((a, index) => a !== arrFlat[index])
+        if (arrFilter.length !== 0) {
+            this.deepOfElement(arrFlat)
+        }
+        if(this.currentDeep > this.deep)
+            this.deep = this.currentDeep
+    }
+    calculateDepth(arr) {
+        let arrFlat = arr.flat()
+        let arrFilter = arr.filter((a, index) => a !== arrFlat[index])
+        if (arrFilter.length !== 0) {
+            this.deep++
+            this.calculateDepth(arrFlat)
+        }
+        return this.deep
+    }
+}
+Object.defineProperty(Array.prototype, 'flat', {
+    value: function(depth = 1) {
+        return this.reduce(function (flat, toFlatten) {
+            return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten);
+        }, []);
+    }
+});
