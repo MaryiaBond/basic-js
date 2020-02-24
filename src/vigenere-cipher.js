2a5a93bb6ff5ed0b1f20f7d4f2944ef4d6169a50
MaryiaBond <dairymolko@gamil.com>
2020-02-24 11:51:15 +0300

    feat: start vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,35 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(decode) {
+        this.alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.!?,:;'/ ";
+        decode ? decode = true : decode = false
+
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(text, key, decode) {
+        function ordA(a) {
+            return a.charCodeAt(0) - 65;
+        }
+        var i = 0, b;
+        key = key.toUpperCase().replace(/[^A-Z]/g, '');
+        return text.toUpperCase().replace(/[^A-Z[^\s\.]+]/g, '').replace(/[A-Z]/g, function(a) {
+            b = key[i++ % key.length];
+            return String.fromCharCode(((ordA(a) + (decode ? 26 - ordA(b) : ordA(b))) % 26 + 65));
+        });
+    }
+
+    decrypt(text, key, decode) {
+        function ordA(a) {
+            return a.charCodeAt(0) - 65;
+        }
+        var i = 0, b;
+        key = key.toUpperCase().replace(/[^A-Z[^\s\.]+]/g, '');
+        return text.toUpperCase().replace(/[^A-Z[^\s\.]+]/g, '').replace(/[A-Z]/g, function(a) {
+            b = key[i++ % key.length];
+            return String.fromCodePoint(((ordA(a) - (decode ? 26 + ordA(b) : ordA(b))) % 26 + 65));
+        });
     }
 }
+
+
 
 module.exports = VigenereCipheringMachine;
