a50455fc684cde69552c47215b565ce139669af4
maria <dairymolko@gmail.com>
2020-02-24 21:25:20 +0300

    feat: vigenere-cepher complete

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,34 +1,36 @@
 class VigenereCipheringMachine {
     constructor(decode) {
-        this.alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.!?,:;'/ ";
-        decode ? decode = true : decode = false
-
+        this.alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      decode ? decode = true : decode = false
+    
     }
-
-    encrypt(text, key, decode) {
-        function ordA(a) {
-            return a.charCodeAt(0) - 65;
+    
+        encrypt(text, key, decode) {
+          let alpha = this.alphabet
+            function ordA(a) {
+                return alpha.indexOf(a);
+            }
+            let i = 0, b;
+            
+            return text.toUpperCase().replace(/[^A-Z[^\s\.]+]/g, '').replace(/[A-Z]/g, function(a) {
+                b = key[i++ % key.length];
+                return String.fromCharCode(((ordA(a) + (decode ? 26 - ordA(b) : ordA(b))) % 26 + 65));
+            });
         }
-        var i = 0, b;
-        key = key.toUpperCase().replace(/[^A-Z]/g, '');
-        return text.toUpperCase().replace(/[^A-Z[^\s\.]+]/g, '').replace(/[A-Z]/g, function(a) {
-            b = key[i++ % key.length];
-            return String.fromCharCode(((ordA(a) + (decode ? 26 - ordA(b) : ordA(b))) % 26 + 65));
-        });
-    }
-
-    decrypt(text, key, decode) {
-        function ordA(a) {
-            return a.charCodeAt(0) - 65;
+    
+        decrypt(text, key, decode) {
+          let alpha = this.alphabet
+            function ordA(a) {
+                return alpha.indexOf(a);
+            }
+            let i = 0, b;
+          
+            return text.toUpperCase().replace(/[^A-Z[^\s\.]+]/g, '').replace(/[A-Z]/g, function(a) {
+                b = key[i++ % key.length]; 
+                return String.fromCharCode(((ordA(a) - (decode ? 26 + ordA(b) : ordA(b))) % 26 + 65));
+            });
         }
-        var i = 0, b;
-        key = key.toUpperCase().replace(/[^A-Z[^\s\.]+]/g, '');
-        return text.toUpperCase().replace(/[^A-Z[^\s\.]+]/g, '').replace(/[A-Z]/g, function(a) {
-            b = key[i++ % key.length];
-            return String.fromCodePoint(((ordA(a) - (decode ? 26 + ordA(b) : ordA(b))) % 26 + 65));
-        });
     }
-}
 
 
 
